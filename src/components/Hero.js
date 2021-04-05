014c3ba5604df09b5ced5e2f31df001ca08aa532
Sergey <kurilovsergey15@gmail.com>
2021-04-05 23:48:34 +0300

    Main page

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,12 +1,45 @@
 import React from "react"
 import Image from "gatsby-image"
-import { Link } from "gatsby"
-import { graphql, useStaticQuery } from "gatsby"
+import {Link} from "gatsby"
+import {graphql, useStaticQuery} from "gatsby"
 import SocialLinks from "../constants/socialLinks"
 // ...GatsbyImageSharpFluid
-
+const query = graphql`
+  {
+    file(relativePath: {eq: "hero-img.png"}) {
+      childImageSharp {
+        fluid {
+        ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+  `
 const Hero = () => {
-  return <h2>hero component</h2>
+    const {
+        file: {
+            childImageSharp: {fluid}
+        }
+    } = useStaticQuery(query)
+    // console.log(data)
+
+
+    return <header className="hero">
+        <div className="section-center hero-center">
+            <article className="hero-info">
+                <div className="underline"> </div>
+                <div>
+                    <h1>I'm Sergey</h1>
+                    <h4>freelance web and mobile dev</h4>
+                    <Link to="/contact" className="btn">
+                        contact me
+                    </Link>
+                    <SocialLinks/>
+                </div>
+            </article>
+            <Image fluid={fluid} className="hero-img"/>
+        </div>
+    </header>
 }
 
 export default Hero
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,16 @@
 import React from "react"
-import { graphql } from "gatsby"
+import {graphql} from "gatsby"
 import Layout from "../components/Layout"
 import Hero from "../components/Hero"
 import Services from "../components/Services"
 import Jobs from "../components/Jobs"
 import Projects from "../components/Projects"
 import Blogs from "../components/Blogs"
-export default () => {
-  return <Layout>
 
-  </Layout>
+export default () => {
+    return <Layout>
+        <Hero/>
+        <Services/>
+    </Layout>
 }
 // ...GatsbyImageSharpFluid
